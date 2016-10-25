c1c8ab48f9ae180442ecd4ecf175b11586d766fe
ecly <elyne@live.dk>
2016-10-26 00:53:39 +0200

    producer stopped printing but implementation seem correct

--- list/list.c
+++ list/list.c
@@ -11,12 +11,13 @@
 #include <pthread.h>
 #include "list.h"
 
-pthread_mutex_t mutex = {0};
+pthread_mutex_t mutex; 
 
 /* list_new: return a new list structure */
 List *list_new(void)
 {
 	List *l;
+	pthread_mutex_init(&mutex, NULL);
 
 	l = (List *) malloc(sizeof(List));
 	l->len = 0;
@@ -25,6 +26,7 @@ List *list_new(void)
 	l->first = l->last = (Node *) malloc(sizeof(Node));
 	l->first->elm = NULL;
 	l->first->next = NULL;
+	printf("we made it here");
 	return l;
 }
 
@@ -32,23 +34,26 @@ List *list_new(void)
 void list_add(List *l, Node *n)
 {
 	pthread_mutex_lock(&mutex);
-	Node *last = l->last;
-	last->next = n;
-	l->last = n;	
-	l->len += 1;
+	l->last->next = n;
+	l->last = n;
+	l->len++;
 	pthread_mutex_unlock(&mutex);
 }
 
 /* list_remove: remove and return the first (non-root) element from list l */
 Node *list_remove(List *l)
 {
+	Node *n;
+
 	pthread_mutex_lock(&mutex);
-	Node *first = l->first;
-	Node *next = first->next;
-	first->next = next->next;
-	l->len -= 1;
-	return next;
+	if(l->len > 0){
+		n = l->first->next;
+		l->first->next = n->next;
+		l->len-- ;
+	}
+
 	pthread_mutex_unlock(&mutex);
+	return n;
 }
 
 /* node_new: return a new node structure */
